135e42fc8e31afd3b6800b6a8cc2685aab04bad2
glob <luisglobanao@gmail.com>
2020-03-25 12:45:37 -0400

    backend

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+const cors = require('cors')
+
+const express = require('express')
+const routes = require('./routes')
+
+const app = express()
+app.use(cors())
+
+// app.use(cors({
+//     origin:'http://meuapp.com'
+// }))
+app.use(express.json())
+app.use(routes)
+
+
+
+app.listen(3333)
